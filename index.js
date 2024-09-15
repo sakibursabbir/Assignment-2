ff0236a20a1fd6768a3f45c7f22d1a3b719a92a1
Sakibur Rahman <sakibur922@gmail.com>
2024-09-15 12:08:28 +0600

    Version 1.0

---
+++ index.js
@@ -0,0 +1,15 @@
+//-------------------------------Answer To Question Number 1------------------------------------//
+function calculateDifference(a, b) {
+    return a - b;
+}
+
+console.log(
+    calculateDifference(8, 1)
+);
+console.log(
+    calculateDifference(1, 8)
+);
+
+
+
+
